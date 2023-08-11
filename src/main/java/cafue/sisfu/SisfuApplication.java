b2feb7a09ef7a08f31b19044e1a78936e1486e8a
otcz <carrillozuletaoscar@gmail.com>
2023-08-11 17:01:51 -0500

    comit

---
+++ src/main/java/cafue/sisfu/SisfuApplication.java
@@ -0,0 +1,13 @@
+package cafue.sisfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cafue/sisfu/SisfuApplicationTests.java
@@ -0,0 +1,13 @@
+package cafue.sisfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisfuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
